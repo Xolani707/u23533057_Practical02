7bd28025badfc06c404b0ff5d6d9b3c73069b88f
Xolani Tobo <u23533057@up.ac.za>
2025-03-04 23:27:11 +0200

    Add the Person Model class

---
+++ u23533057_Practical02/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u23533057_Practical02.Models
+{
+	public class PersonModel
+	{
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Current Age")]
+        public int Age { get; set; } = 0;
+
+        [Display(Name = "Living Status")]
+        public bool IsAlive { get; set; } = true;
+    }
+}
